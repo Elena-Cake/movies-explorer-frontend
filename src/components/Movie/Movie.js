f076fae8c092a11bf859aff9c260a4cf43fd9237
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-04-13 23:43:36 +0300

    feat movie card

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,37 @@
+
+import './Movie.css';
+
+const data = {
+  country: "США",
+  created_at: "2020-11-23T14:12:21.376Z",
+  description: "В конце 1960-х группа «Роллинг Стоунз», несмотря на все свои мегахиты и сверхуспешные концертные туры, была разорена.",
+  director: "Стивен Кайак ",
+  duration: 61,
+  id: 1,
+  image: {
+    previewUrl: "https://cdn5.vedomosti.ru/image/2019/7u/1eejw4/original-1tbo.jpg"
+  },
+  nameEN: "Stones in Exile",
+  nameRU: "33 слова о дизайне",
+  trailerLink: "https://www.youtube.com/watch?v=UXcqcdYABFw",
+  updated_at: "2020-11-23T14:12:21.376Z",
+  year: "2010"
+}
+
+function Movie({ data }) {
+
+  const duration = `${Math.floor(data.duration / 60) > 0 ? `${Math.floor(data.duration / 60)}ч ` : ''}${data.duration % 60}м`
+
+  return (
+    <div className="movie">
+      <div className='movie__info'>
+        <h3 className='movie__name'>{data.nameRU}</h3>
+        <p className='movie__duration'>{duration}</p>
+        <button className='movie__flag movie__flag_active button'></button>
+      </div>
+      <img className='movie__foto' src={data.image.previewUrl} alt='скриншот фильма' />
+    </div>
+  );
+}
+
+export default Movie;
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,42 @@
 
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
+import Movie from '../Movie/Movie';
 
 function Movies() {
+  const movies = [
+    {
+      country: "США",
+      created_at: "2020-11-23T14:12:21.376Z",
+      description: "В конце 1960-х группа «Роллинг Стоунз», несмотря на все свои мегахиты и сверхуспешные концертные туры, была разорена.",
+      director: "Стивен Кайак ",
+      duration: 67,
+      id: 1,
+      image: {
+        previewUrl: "https://cdn5.vedomosti.ru/image/2019/7u/1eejw4/original-1tbo.jpg"
+      },
+      nameEN: "Stones in Exile",
+      nameRU: "33 слова о дизайне",
+      trailerLink: "https://www.youtube.com/watch?v=UXcqcdYABFw",
+      updated_at: "2020-11-23T14:12:21.376Z",
+      year: "2010"
+    }
+  ];
+
+  // const movieElements = movies.map((movie) => <Movie data={movie} />)
+
+  const movieElements = [];
+  for (let i = 0; i < 30; i++) {
+    movieElements.push(movies.map((movie) => <Movie data={movie} />))
+  }
+
+
   return (
     <div className="movies">
       <SearchForm />
+      <div className='movies__container'>
+        {movieElements}
+      </div>
     </div>
   );
 }
