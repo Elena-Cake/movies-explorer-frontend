0dae9ac198d21ed6cb3e1c11428ef11ebfe60b8e
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-04-13 16:11:14 +0300

    feat footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,13 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="main">
-
+    <div className="footer">
+      <div className='footer__container'>
+        <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <p className='footer__year'>&#169;2023</p>
+        <p className='footer__yp'>Яндекс.Практикум</p>
+        <p className='footer__git'>Github</p>
+      </div>
     </div>
   );
 }
