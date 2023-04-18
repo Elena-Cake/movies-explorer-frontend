0df305ffdb51c17b142ded98166729a1332b3bdc
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-04-18 17:12:37 +0300

    profile button edit

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,14 @@
 
+import { useState } from 'react';
 import './Profile.css';
 
 function Profile() {
+
+  const [isEditActive, setIsEditActive] = useState(false)
+
+  const activateEdit = () => {
+    setIsEditActive(!isEditActive)
+  }
 
   // Тестовые данные для заполнения
   const data = {
@@ -16,17 +23,30 @@ function Profile() {
         <div className='profile__rows'>
           <div className='profile__row'>
             <p className='row__title'>Имя</p>
-            <input className='row__data' value={data.name} placeholder='Введите имя' readonly />
+            {!isEditActive ?
+              <p className='row__data'>{data.name}</p>
+              :
+              <input className='row__data' placeholder='Введите имя' value={data.name} />
+            }
           </div>
           <div className='profile__row'>
             <p className='row__title'>E-mail</p>
-            <input className='row__data' value={data.email} placeholder='Введите почту' readonly />
+            {!isEditActive ?
+              <p className='row__data'>{data.email}</p>
+              :
+              <input className='row__data' placeholder='Введите почту' value={data.email} />
+            }
           </div>
         </div>
-        <div className='profile__settings'>
-          <button className='profile__edit-button button'>Редактировать</button>
-          <button className='profile__exit-button button'>Выйти из аккаунта</button>
-        </div>
+        {!isEditActive ?
+          <div className='profile__settings'>
+
+            <button className='profile__edit-button button' onClick={activateEdit}>Редактировать</button>
+            <button className='profile__exit-button button'>Выйти из аккаунта</button>
+          </div>
+          :
+          <button className='profile__save-button button' onClick={activateEdit}>Сохранить</button>
+        }
       </div>
     </div >
   );
