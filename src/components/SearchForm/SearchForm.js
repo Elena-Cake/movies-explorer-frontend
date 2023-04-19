8939477b2eb772a5541cd9b3e33fad45d846ac60
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-04-19 11:46:33 +0300

    feat toggle filter icons

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,14 @@
 
+import { useState } from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
+
+  const [isShortFilmsActive, setIsShortFilmsActive] = useState(false);
+  const toggleShortFilm = () => {
+    setIsShortFilmsActive(!isShortFilmsActive)
+  }
+
   return (
     <div className="search">
       <div className='search__row'>
@@ -9,7 +16,7 @@ function SearchForm() {
         <button className='search__button button'>Поиск</button>
       </div>
       <div className='search__filters'>
-        <div className='filter__icon filter__icon_active'></div>
+        <button className={`filter__icon ${isShortFilmsActive ? 'filter__icon_active' : ''}`} onClick={toggleShortFilm}></button>
         <p className='filter__text'>Короткометражки</p>
       </div>
       <div className='search__bottom'></div>
