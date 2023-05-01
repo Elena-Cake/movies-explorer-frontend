559cd7f8c3b38d258a957b3933abaef22d64588c
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-05-01 12:27:21 +0300

    fix pattern, fix constants, fix signin signup path

--- src/components/Auth/AuthForm/FormRow/FormRow.js
+++ src/components/Auth/AuthForm/FormRow/FormRow.js
@@ -2,8 +2,9 @@
 import './FormRow.css';
 import { useFormAndValidation } from '../../../../hooks/useValidationForm'
 import { useEffect } from 'react';
+import { PATTERN_URL } from '../../../../constans/inputsData';
 
-function FormRow({ data, onChangeButtonActive, onSetValues }) {
+function FormRow({ data, onChangeButtonActive }) {
 
   const { values, handleChange, errors, isValid } = useFormAndValidation();
   const nameInput = data.name;
@@ -15,15 +16,29 @@ function FormRow({ data, onChangeButtonActive, onSetValues }) {
   return (
     <li className="row">
       <p className='row__title'>{data.title}</p>
-      <input
-        className={`row__input ${errors[nameInput] ? 'row__input_error' : ''}`}
-        name={data.name}
-        placeholder={`Введите ${data.title}`}
-        value={values[nameInput] || ''}
-        type={data.validation.type || "text"}
-        required={data.validation.required ? data.validation.required : false}
-        onChange={handleChange}
-      />
+      {data.validation.type === 'email' ?
+        <input
+          className={`row__input ${errors[nameInput] ? 'row__input_error' : ''}`}
+          name={data.name}
+          placeholder={`Введите ${data.title}`}
+          value={values[nameInput] || ''}
+          type={data.validation.type}
+          pattern={PATTERN_URL}
+          required={data.validation.required ? data.validation.required : false}
+          onChange={handleChange}
+        />
+        :
+        <input
+          className={`row__input ${errors[nameInput] ? 'row__input_error' : ''}`}
+          name={data.name}
+          placeholder={`Введите ${data.title}`}
+          value={values[nameInput] || ''}
+          type={data.validation.type || 'text'}
+          required={data.validation.required}
+          onChange={handleChange}
+        />
+      }
+
       <span className={`row__error ${errors[nameInput] ? 'row__error_active' : ''}`}>{errors[nameInput]}</span>
     </li>
   );
--- src/constans/inputsData.js
+++ src/constans/inputsData.js
@@ -1,3 +1,5 @@
+export const PATTERN_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
 export const textsRegister = {
     title: "Добро пожаловать!",
     button: "Зарегистрироваться",
